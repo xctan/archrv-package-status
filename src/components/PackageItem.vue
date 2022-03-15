884137983cdf272415c3267c84f58425c5da5e8f
xctan <sterprim@outlook.com>
2022-03-15 16:13:00 +0800

    add PackageItem.vue

---
+++ src/components/PackageItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="package-item">
+    <span class="package-c1">{{ pack.name }}</span>
+    <span class="package-c2">{{ pack.user }}</span>
+    <span class="package-c3">
+      <a v-if="pack.workUrl" :href="pack.workUrl" target="_blank">
+        {{ pack.work }}
+      </a>
+      <span v-else>{{ pack.work }}</span>
+    </span>
+    <span class="package-c4">
+      <span
+          v-for="mark in pack.marks"
+          :title="mark.more"
+          :key="mark.desc"
+          class="package-mark"
+      >
+        {{ mark.desc }}{{ mark.more ? '*' : '' }}
+      </span>
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+/* eslint-disable no-unused-vars */
+const props = defineProps({
+  pack: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.package-item {
+  display: grid;
+  grid-template-columns: minmax(0, 3fr) 1fr 1fr minmax(0, 4fr);
+
+}
+
+.package-c1 {
+  overflow: hidden;
+}
+
+.package-c2 {
+
+}
+
+.package-c3 {
+
+}
+
+.package-c4 {
+
+}
+
+.package-mark {
+  margin-inline: 4px;
+  padding-inline: 5px;
+  outline: darkred solid 1px;
+  border-radius: 1em;
+}
+</style>
--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,10 +1,76 @@
 <template>
   <div>Page Not Found</div>
+  <package-item :pack="testItem"></package-item>
+  <package-item :pack="testItem"></package-item>
+  <package-item :pack="anotherItem"></package-item>
+  <package-item :pack="testItem"></package-item>
+  <package-item :pack="item3"></package-item>
+  <package-item :pack="item4"></package-item>
+  <package-item :pack="item5"></package-item>
+  <package-item :pack="item6"></package-item>
 </template>
 
 <script>
+import PackageItem from '@/components/PackageItem'
+
 export default {
-  name: 'NotFound'
+  name: 'NotFound',
+  components: {
+    PackageItem
+  },
+  data () {
+    return {
+      testItem: {
+        name: 'test',
+        user: 'null',
+        work: 'working',
+        workUrl: null,
+        marks: [
+          { desc: 'dep outdated', more: 'no more info(' },
+          { desc: 'stuck', more: 'no jit support :(' }
+        ]
+      },
+      anotherItem: {
+        name: 'AVeryLongNameForABasicPackageItemWithMuchDescriptionAndTag',
+        user: 'null',
+        work: 'working',
+        workUrl: null,
+        marks: [
+          { desc: 'dep outdated', more: 'no more info(' },
+          { desc: 'stuck', more: 'no jit support :(' }
+        ]
+      },
+      item3: {
+        name: 'merged',
+        user: 'null',
+        work: 'merged',
+        workUrl: null,
+        marks: []
+      },
+      item4: {
+        name: 'stuck',
+        user: 'null',
+        work: '',
+        workUrl: null,
+        marks: [
+          { desc: 'stuck', more: null }
+        ]
+      },
+      item5: {
+        name: 'PullRequested',
+        user: '',
+        work: 'pull requested',
+        workUrl: 'https://github.com/felixonmars/archriscv-packages/pull/970',
+        marks: []
+      },
+      item6: {
+        name: 'memcached',
+        user: '',
+        work: '',
+        marks: []
+      }
+    }
+  }
 }
 </script>
 
